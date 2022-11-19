4ab70f4799896b04862a0c2430c314fb1a0b5468
SorinGFS <sorin.gfs@protonmail.com>
2022-11-19 19:50:57 +0200

    bump mongodb to 4.12.0

--- server/db/model/mongodb/index.js
+++ server/db/model/mongodb/index.js
@@ -19,26 +19,26 @@ class Model extends DB {
     getController = (controller, db) => this.getDb(db).then((db) => db.collection(controller));
     select = (table, fromDb) => this.getDb(fromDb).then((db) => db.collection(table)); // alias
     // interface methods to the driver
-    exec() {
-        return this._chain(this._exec, arguments);
+    exec(...args) {
+        return this._chain(this._exec, ...args);
     }
-    _exec(fn, args) {
-        this[fn].apply(this, args ? Array.prototype.slice.call(args) : undefined);
+    _exec(fn, ...args) {
+        this[fn].apply(this, args);
     }
-    returnArg() {
-        return this._chain(this._returnArg, arguments);
+    returnArg(...args) {
+        return this._chain(this._returnArg, ...args);
     }
-    _returnArg() {
-        return Array.prototype.slice.call(arguments)[0];
+    _returnArg(...args) {
+        return args[0];
     }
-    returnArgs() {
-        return this._chain(this._returnArgs, arguments);
+    returnArgs(...args) {
+        return this._chain(this._returnArgs, ...args);
     }
-    _returnArgs() {
-        return Array.prototype.slice.call(arguments);
+    _returnArgs(...args) {
+        return args;
     }
-    restClient() {
-        return this._chain(this._init, arguments);
+    restClient(...args) {
+        return this._chain(this._init, ...args);
     }
     async _init(connection) {
         if (!this.isConnected || connection) {
@@ -47,8 +47,8 @@ class Model extends DB {
             this.isConnected = true;
         }
     }
-    db() {
-        return this._chain(this._initDb, arguments);
+    db(...args) {
+        return this._chain(this._initDb, ...args);
     }
     async _initDb(db, connection) {
         if (!this.isDbConnected || db) {
@@ -58,8 +58,8 @@ class Model extends DB {
             this.isDbConnected = true;
         }
     }
-    admin() {
-        return this._chain(this._initAdmin, arguments);
+    admin(...args) {
+        return this._chain(this._initAdmin, ...args);
     }
     async _initAdmin(db, connection) {
         if (!this.isAdminConnected || connection || db) {
@@ -69,8 +69,8 @@ class Model extends DB {
             this.isAdminConnected = true;
         }
     }
-    controller() {
-        return this._chain(this._initController, arguments);
+    controller(...args) {
+        return this._chain(this._initController, ...args);
     }
     async _initController(controller, db, connection) {
         if (!this.isControllerConnected || controller) {
@@ -82,8 +82,8 @@ class Model extends DB {
         }
     }
     // not in the restApi
-    debug() {
-        return this._chain(this._debug, arguments);
+    debug(...args) {
+        return this._chain(this._debug, ...args);
     }
     async _debug() {
         console.dir(this, { depth: false });
@@ -92,16 +92,16 @@ class Model extends DB {
         await this.client.close();
     }
     // not in the restApi
-    command() {
-        return this._chain(this._command, arguments);
+    command(...args) {
+        return this._chain(this._command, ...args);
     }
     async _command(command, options) {
         if (!this.isDbConnected) await this._initDb();
         return await this._db.command({ ...command }, { ...options });
     }
     // not in the restApi
-    listDatabases() {
-        return this._chain(this._listDatabases, arguments);
+    listDatabases(...args) {
+        return this._chain(this._listDatabases, ...args);
     }
     async _listDatabases(listDatabasesOptions) {
         if (!this.isAdminConnected) await this._initAdmin();
@@ -110,44 +110,44 @@ class Model extends DB {
     async listAuthorizedDatabases() {
         return await this.listDatabases({ nameOnly: true, authorizedDatabases: true }).then((obj) => [].concat(obj.databases.map((item) => item.name)));
     }
-    dropDatabase() {
-        return this._chain(this._dropDatabase, arguments);
+    dropDatabase(...args) {
+        return this._chain(this._dropDatabase, ...args);
     }
     async _dropDatabase(commandOperationOptions) {
         if (!this.isDbConnected) await this._initDb();
         return await this._db.dropDatabase({ ...commandOperationOptions });
     }
-    addAdmin() {
-        return this._chain(this._addAdmin, arguments);
+    addAdmin(...args) {
+        return this._chain(this._addAdmin, ...args);
     }
     async _addAdmin(username, password, addUserOptions) {
         if (!this.isAdminConnected) await this._initAdmin();
         return await this._db._admin.addUser(username, password, { ...addUserOptions });
     }
-    removeAdmin() {
-        return this._chain(this._removeAdmin, arguments);
+    removeAdmin(...args) {
+        return this._chain(this._removeAdmin, ...args);
     }
     async _removeAdmin(username, commandOperationOptions) {
         if (!this.isAdminConnected) await this._initAdmin();
         return await this._db._admin.removeUser(username, { ...commandOperationOptions });
     }
-    addUser() {
-        return this._chain(this._addUser, arguments);
+    addUser(...args) {
+        return this._chain(this._addUser, ...args);
     }
     async _addUser(username, password, addUserOptions) {
         if (!this.isDbConnected) await this._initDb();
         return await this._db.addUser(username, password, { ...addUserOptions });
     }
-    removeUser() {
-        return this._chain(this._removeUser, arguments);
+    removeUser(...args) {
+        return this._chain(this._removeUser, ...args);
     }
     async _removeUser(username, commandOperationOptions) {
         if (!this.isDbConnected) await this._initDb();
         return await this._db.removeUser(username, { ...commandOperationOptions });
     }
     // not in the restApi
-    listControllers() {
-        return this._chain(this._listControllers, arguments);
+    listControllers(...args) {
+        return this._chain(this._listControllers, ...args);
     }
     async _listControllers(filter, listCollectionsOptions) {
         if (!this.isDbConnected) await this._initDb();
@@ -156,49 +156,49 @@ class Model extends DB {
     async listAuthorizedControllers() {
         return await this.listControllers({}, { nameOnly: true, authorizedCollections: true }).then((col) => [].concat(col.map((item) => item.name)));
     }
-    createController() {
-        return this._chain(this._createController, arguments);
+    createController(...args) {
+        return this._chain(this._createController, ...args);
     }
     async _createController(name, createCollectionOptions) {
         if (!this.isDbConnected) await this._initDb();
         return await this._db.createCollection(name, { ...createCollectionOptions });
     }
-    setControllerOptions() {
-        return this._chain(this._setControllerOptions, arguments);
+    setControllerOptions(...args) {
+        return this._chain(this._setControllerOptions, ...args);
     }
     async _setControllerOptions(collectionOptions, commandOptions) {
         return await this._command({ collMod: this.connection.controller, ...collectionOptions }, { ...commandOptions });
     }
-    renameController() {
-        return this._chain(this._renameController, arguments);
+    renameController(...args) {
+        return this._chain(this._renameController, ...args);
     }
     async _renameController(fromCollection, toCollection, renameOptions) {
         if (!this.isDbConnected) await this._initDb();
         return await this._db.renameCollection(fromCollection, toCollection, { ...renameOptions });
     }
-    dropController() {
-        return this._chain(this._dropController, arguments);
+    dropController(...args) {
+        return this._chain(this._dropController, ...args);
     }
     async _dropController(collectionName, commandOperationOptions) {
         if (!this.isDbConnected) await this._initDb();
         return await this._db.dropCollection(collectionName, { ...commandOperationOptions });
     }
-    listIndexes() {
-        return this._chain(this._listIndexes, arguments);
+    listIndexes(...args) {
+        return this._chain(this._listIndexes, ...args);
     }
     async _listIndexes() {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.listIndexes().toArray();
     }
-    createIndex() {
-        return this._chain(this._createIndex, arguments);
+    createIndex(...args) {
+        return this._chain(this._createIndex, ...args);
     }
     async _createIndex(indexSpecification, createIndexesOptions) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.createIndex([...indexSpecification], { ...createIndexesOptions });
     }
-    dropIndex() {
-        return this._chain(this._dropIndex, arguments);
+    dropIndex(...args) {
+        return this._chain(this._dropIndex, ...args);
     }
     async _dropIndex(index) {
         if (!this.isControllerConnected) await this._initController();
@@ -213,96 +213,96 @@ class Model extends DB {
     async info() {
         return await this.listControllers({ name: this.connection.controller }).then((c) => c[0].info);
     }
-    count() {
-        return this._chain(this._countDocuments, arguments);
+    count(...args) {
+        return this._chain(this._countDocuments, ...args);
     }
     async _countDocuments(filter, countDocumentsOptions) {
         if (!this.isControllerConnected) await this._initController();
         return { matchedCount: await this._db._collection.countDocuments({ ...filter }, { ...countDocumentsOptions }) };
     }
-    distinct() {
-        return this._chain(this._distinct, arguments);
+    distinct(...args) {
+        return this._chain(this._distinct, ...args);
     }
     async _distinct(key, filter, commandOperationOptions) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.distinct(key, { ...filter }, { ...commandOperationOptions });
     }
-    aggregate() {
-        return this._chain(this._aggregate, arguments);
+    aggregate(...args) {
+        return this._chain(this._aggregate, ...args);
     }
     async _aggregate(pipeline, aggregateOptions) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.aggregate([...pipeline], { ...aggregateOptions }).toArray();
     }
     // not in the restApi
-    bulkWrite() {
-        return this._chain(this._bulkWrite, arguments);
+    bulkWrite(...args) {
+        return this._chain(this._bulkWrite, ...args);
     }
     async _bulkWrite(operations, bulkWriteOptions) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.bulkWrite([...operations], { ...bulkWriteOptions });
     }
     // not in the restApi
-    watch() {
-        return this._chain(this._watch, arguments);
+    watch(...args) {
+        return this._chain(this._watch, ...args);
     }
     async _watch(pipeline, changeStreamOptions) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.watch([...pipeline], { ...changeStreamOptions });
     }
-    findOne() {
-        return this._chain(this._findOne, arguments);
+    findOne(...args) {
+        return this._chain(this._findOne, ...args);
     }
     async _findOne(filter, options) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.findOne({ ...filter }, { ...options });
     }
-    findMany() {
-        return this._chain(this._findMany, arguments);
+    findMany(...args) {
+        return this._chain(this._findMany, ...args);
     }
     async _findMany(filter, options) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.find({ ...filter }, { ...options }).toArray();
     }
-    find() {
-        return this._chain(this._find, arguments);
+    find(...args) {
+        return this._chain(this._find, ...args);
     }
     async _find(filter, options) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.find({ ...filter }, { ...options });
     }
-    insertOne() {
-        return this._chain(this._insertOne, arguments);
+    insertOne(...args) {
+        return this._chain(this._insertOne, ...args);
     }
     async _insertOne(document, options) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.insertOne({ ...document }, { ...options });
     }
-    insertMany() {
-        return this._chain(this._insertMany, arguments);
+    insertMany(...args) {
+        return this._chain(this._insertMany, ...args);
     }
     async _insertMany(documents, options) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.insertMany([...documents], { ...options });
     }
-    updateOne() {
-        return this._chain(this._updateOne, arguments);
+    updateOne(...args) {
+        return this._chain(this._updateOne, ...args);
     }
     async _updateOne(filter, update, options) {
         if (options) delete options.upsert;
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.updateOne({ ...filter }, { $set: { ...update } }, { ...options });
     }
-    updateMany() {
-        return this._chain(this._updateMany, arguments);
+    updateMany(...args) {
+        return this._chain(this._updateMany, ...args);
     }
     async _updateMany(filter, update, options) {
         if (options) delete options.upsert;
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.updateMany({ ...filter }, { $set: { ...update } }, { ...options });
     }
-    upsertOne() {
-        return this._chain(this._upsertOne, arguments);
+    upsertOne(...args) {
+        return this._chain(this._upsertOne, ...args);
     }
     async _upsertOne(filter, update, options) {
         if (options) delete options.upsert;
@@ -316,22 +316,22 @@ class Model extends DB {
         }
         return await this.bulkWrite(operations);
     }
-    deleteOne() {
-        return this._chain(this._deleteOne, arguments);
+    deleteOne(...args) {
+        return this._chain(this._deleteOne, ...args);
     }
     async _deleteOne(filter, options) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.deleteOne({ ...filter }, { ...options });
     }
-    deleteMany() {
-        return this._chain(this._deleteMany, arguments);
+    deleteMany(...args) {
+        return this._chain(this._deleteMany, ...args);
     }
     async _deleteMany(filter, options) {
         if (!this.isControllerConnected) await this._initController();
         return await this._db._collection.deleteMany({ ...filter }, { ...options });
     }
-    _chain(fn, args) {
-        this.p = this.p.then(() => fn.apply(this, Array.prototype.slice.call(args)));
+    _chain(fn, ...args) {
+        this.p = this.p.then(() => fn.apply(this, args));
         return this;
     }
     then(a, b) {
